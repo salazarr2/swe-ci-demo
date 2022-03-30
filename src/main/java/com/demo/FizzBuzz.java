038be07f853c641c4b5281f3501dd250991d5233
salazarr2 <71840043+salazarr2@users.noreply.github.com>
2022-03-30 14:44:39 -0400

    Update FizzBuzz.java

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -4,14 +4,42 @@ public class FizzBuzz
 {
     String processNumber(int num)
 	{	
-    	// Copy your code here
-		return "";
+    	StringBuilder output = new StringBuilder("");
+        Boolean both = false;
+        if (num <= 0) {
+          return output.toString();
+        }
+        if (num % 3 != 0 && num % 5 != 0) { 
+          return String.valueOf(num);
+        }
+        if (num % 3 == 0) {
+          output.append("Fizz");
+          both = true;
+        }
+        if (num % 5 == 0) {
+          if (both) {
+            output.append(" ");
+          }
+          output.append("Buzz");
+        }
+        
+        return output.toString();
 	}
 	
 	String processNumbers (int[] numList)
 	{
-    	// Copy your code here
-		return "";
+    	StringBuilder output = new StringBuilder("");
+        for (int i = 0; i < nums.length; i++) {
+          int test = nums[i];
+          if (test <= 0) {
+            continue;
+          }
+          output.append(processNumber(test));
+          if (i != nums.length -1) {
+            output.append(", ");
+          }
+        }
+        return output.toString();
 	}
 
 }
